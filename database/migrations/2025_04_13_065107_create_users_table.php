f31c01c36f0bb1732c4a710d4b2996ab5f4cdc57
Aven2002 <avending2002@outlook.com>
2025-04-13 15:59:59 +0800

    Migration Files Completed

---
+++ database/migrations/2025_04_13_065107_create_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('userRole',20)->default('User');
+            $table->string('email',255)->unique();
+            $table->string('username',30)->unique();
+            $table->string('password');
+            $table->date('dob');
+            $table->json('security_answers');
+            $table->string('imgPath')->default('images/Profile_Img/Profile_Img_Default.png');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
