1bd621deb96c9e438f94af32127313dfda3e31fd
Aven2002 <avending2002@outlook.com>
2025-04-14 01:02:02 +0800

    AddressController Completed

---
+++ app/Http/Controllers/AddressController.php
@@ -0,0 +1,217 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Validation\ValidationException;
+use Illuminate\Http\Request;
+use App\Models\Address;
+
+class AddressController extends Controller
+{
+    /**
+     * Retrieve all address records
+     */
+    public function index()
+    {
+        try{
+            $address = Address::all();
+
+            if ($address->isEmpty())
+            {
+                return response()->json([
+                    'message'=> 'The table is empty'
+                ],404);
+            }
+
+            return response()->json([
+                'message'=>'All address records retrieved successfully',
+                'address'=>$address
+            ],200);
+
+        }catch(\Exception $e)
+        {
+            return response()->json([
+                'message'=>'Something went wrong',
+                'error'=>$e->getMessage()
+            ],500);
+        }
+    }
+
+    /**
+     * Retrieve specific address record
+     */
+    public function show($id)
+    {
+        try{
+            $address = Address::find($id);
+
+            if(!$address)
+            {
+                return response()->json([
+                    'message'=>'Address record not found'
+                ],404);
+            }
+
+            return response()->json([
+                'message'=>'Address record retrieve successfully',
+                'address'=>$address
+            ],200);
+
+        }catch(\Exception $e)
+        {
+            return response()->json([
+                'message'=>'Something went wrong',
+                'error'=>$e->getMessage()
+            ],500);
+        }
+    }
+
+    /**
+     * Retrieve the list of adressess own by the user
+     */
+    public function getUserAddress($id)
+    {
+        try{
+            $addresses = Address::where('user_id',$id)->get();
+
+            if($addresses->isEmpty())
+            {
+                return response()->json([
+                    'message'=> 'No address found for this user'
+                ],404);
+            }
+
+            return response()->json([
+                'message'=>'User addresses retrieved successfully',
+                'addresses'=>$addresses
+            ],200);
+
+        }catch(\Exception $e)
+        {
+            return response()->json([
+                'message'=>'Something went wrong',
+                'error'=>$e->getMessage()
+            ],500);
+        }
+    }
+
+    /**
+     * Remove the specified address 
+     */
+    public function destroy($id)
+    {
+        try{
+            $address = Address::find($id);
+
+            if(!$address)
+            {
+                return response()->json([
+                    'message'=>'Address record not found'
+                ],404);
+            }
+
+            $address->delete();
+
+            return response()->json([
+                'message'=>'Address record deleted successfully'
+            ],200);
+
+        }catch(\Exception $e)
+        {
+            return response()->json([
+                'message'=>'Something went wrong',
+                'error'=>$e->getMessage()
+            ],500);
+        }
+    }
+
+    /**
+     * Store a newly created address
+     */
+    public function store(Request $request)
+    {
+        try{
+
+            $validatedData = $request->validate([
+                'user_id'=>'required|integer',
+                'name'=>'required|string|max:50',
+                'phoneNo' => 'required|string|max:20',
+                'addressLine' => 'required|string',
+                'city' => 'required|string|max:50',
+                'state' => 'required|string|max:50',
+                'postcode' => 'required|digits:5',
+                'isDefault' => 'boolean'
+            ]);
+
+            $address = Address::create($validatedData);
+
+            return response()->json([
+                'message'=>'Address record created successfully',
+                'address'=>$address
+            ],201);
+
+        }catch(ValidationException $e)
+        {
+            return response()->json([
+                'message'=>'Validation error',
+                'errors'=>$e->errors()
+            ],422);
+
+        }catch(\Exception $e)
+        {
+            return response()->json([
+                'message'=>'Something went wrong',
+                'error'=>$e->getMessage()
+            ],500);
+        }
+    }
+
+    /**
+     * Update the specific address record
+     */
+    public function update(Request $request, $id)
+    {
+        try{
+            $validatedData = $request->validate([
+                'name' => 'required|string|max:50',
+                'phoneNo' => 'required|string|max:20',
+                'addressLine' => 'required|string',
+                'city' => 'required|string|max:50',
+                'state' => 'required|string|max:50',
+                'postcode' => 'required|digits:5',
+                'isDefault' => 'boolean'
+            ]);
+
+            // Find the address by its ID
+            $address = Address::find($id);
+
+            // Check if the address exists
+            if (!$address) {
+                return response()->json([
+                    'message' => 'Address not found'
+                ], 404);
+            }
+
+            $address->update($validatedData);
+
+            return response()->json([
+                'message'=>'Address record updated successfully',
+                'address'=>$address
+            ],200);
+
+        }catch(ValidationException $e)
+        {
+            return response()->json([
+                'message'=>'Validation error',
+                'errors'=>$e->errors()
+            ],422);
+        }catch(\Exception $e)
+        {
+            return response()->json([
+                'message'=>'Something went wrong',
+                'error'=>$e->getMessage()
+            ],500);
+        }
+    }
+    
+}
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AddressController;
 
 Route::get('/test', function () {
     return ('welcome');
 });
+
+//Address
+Route::resource('address',AddressController::class);
+Route::get('/address/{id}/user',[AddressController::class,'getUserAddress']);
+
