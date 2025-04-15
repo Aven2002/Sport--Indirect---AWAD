26de731e90943d0b81f9150c7b2cfb93ed72f39f
Aven2002 <avending2002@outlook.com>
2025-04-15 14:19:01 +0800

    View Controller Completed

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ViewController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/{view}',[ViewController::class,'show']);
